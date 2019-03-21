9ea431294e46533e6be692766e2b95f2497b9f83
cWenyu <45832472+cWenyu@users.noreply.github.com>
2019-03-21 09:32:35 +0000

    server and client

---
+++ Client.java
@@ -0,0 +1,93 @@
+package BusinessObjects;
+
+/**
+ * CLIENT February 2019 DL 08/03/19
+ *
+ * This Client program asks the user to input commands to be sent to the server.
+ *
+ * There are only two valid commands in the protocol: "Time" and "Echo"
+ *
+ * If user types "Time" the server should reply with the current server time.
+ *
+ * If the user types "Echo" followed by a message, the server will echo back the
+ * message. e.g. "Echo Nice to meet you"
+ *
+ * If the user enters any other input, the server will not understand, and will
+ * send back a message to the effect.
+ *
+ * NOte: You must run the server before running this the client. (Both the
+ * server and the client will be running together on this computer)
+ */
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
+    public void start() {
+        Scanner in = new Scanner(System.in);
+        try {
+            Socket socket = new Socket("localhost", 8080);  // connect to server socket, and open new socket
+
+            System.out.println("Client: Port# of this client : " + socket.getLocalPort());
+            System.out.println("Client: Port# of Server :" + socket.getPort());
+
+            System.out.println("Client: This Client is running and has connected to the server");
+
+            String message = "please enter command:";
+            System.out.print(message);
+            String command;
+
+            OutputStream os = socket.getOutputStream();
+
+            PrintWriter socketWriter = new PrintWriter(os, true);
+
+            Scanner socketReader = new Scanner(socket.getInputStream());
+            labelB:
+            while (in.hasNextLine()) {
+                command = in.nextLine();  // read a command from the user
+
+                String[] lineWords = command.split(" ");
+                String subCommand = lineWords[0].toUpperCase();
+
+                socketWriter.println(command);
+
+                switch (subCommand) {
+                    case "TESTMOVIES":
+                    case "FINDMOVIEBYID":
+                    case "FINDMOVIEBYTITLE":
+                    case "FINDMOVIEBYYEAR":
+                    case "FINDMOVIEBYDIRECTOR":
+                    case "TOPTENMOVIES":
+                        String m = socketReader.nextLine();
+                        System.out.println("movies:\"" + m + "\"");
+                        break;
+
+                    case "DELETEMOVIE":
+                    case "UPDATEMOVIETITLE":
+                    case "INSERTNEWMOVIE":
+                        String str = socketReader.nextLine();
+                        System.out.println("Message:" + str);
+                        break;
+
+                    default:
+                        break labelB;
+                }
+                System.out.print(message);
+            }
+            socketWriter.close();
+            socketReader.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Client message: IOException: " + e);
+        }
+    }
+}
